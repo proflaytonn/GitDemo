33cee7ee2acbc92e6cba03da449041251d11002e
Jessen <jessenkemal@yahoo.com>
2022-11-16 17:29:56 +0700

    push to the branch

--- src/dataProvide.java
+++ src/dataProvide.java
@@ -8,9 +8,42 @@ public class dataProvide {
 	@Test
 	public void testCaseData()
 	{
-		//ini project error
+		//ini project di developBranch
+		//ini project di developBranch
+		//ini project di developBranch
 	}
 	
+	@Test
+	public void testCaseData2()
+	{
+		//ini project error
+		
+		//ini project di developBranch
+	}
+	
+	@Test
+	public void testCaseData3()
+	{
+		//ini project error
+		
+		//ini project di developBranch
+	}
+	
+	@Test
+	public void testCaseData4()
+	{
+		//ini project error
+		
+		//ini project di developBranch
+	}
+	
+	@Test
+	public void testCaseData5()
+	{
+		//ini project error
+		
+		//ini project di developBranch
+	}
 	
 	
 	
