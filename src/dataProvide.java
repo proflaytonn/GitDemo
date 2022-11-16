2aa454f2183e25e9e4116dbe62f26c600f2a34b2
Jessen <jessenkemal@yahoo.com>
2022-11-16 11:38:39 +0700

    first time commit GIT

---
+++ src/dataProvide.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class dataProvide {
+	
+	
+	
+	@Test
+	public void testCaseData()
+	{
+		
+	}
+	
+	
+	
+	
+	@DataProvider
+	public void getData()
+	{
+		Object[][] data = {};
+	}
+	
+}
